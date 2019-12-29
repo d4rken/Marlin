03727c4f8cf5775f6febd5a5fa4795fb5721e037
darken <darken@darken.eu>
2019-12-29 23:30:16 +0100

    Tweaks

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -725,10 +725,10 @@
 
 #if ENABLED(MESH_BED_LEVELING) || ENABLED(AUTO_BED_LEVELING_UBL)
   // Override the mesh area if the automatic (max) area is too large
-  //#define MESH_MIN_X MESH_INSET
-  //#define MESH_MIN_Y MESH_INSET
-  //#define MESH_MAX_X X_BED_SIZE - (MESH_INSET)
-  //#define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
+  #define MESH_MIN_X MESH_INSET
+  #define MESH_MIN_Y MESH_INSET + 13 // Probe offset, can't go below min stop
+  #define MESH_MAX_X X_BED_SIZE - (MESH_INSET) - 15 // Probe offset, can't go further right
+  #define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
 #endif
 
 // @section extras
