ec8a8ca05826053d150a27daecd8d13420dbdb22
darken <darken@darken.eu>
2020-01-07 15:08:39 +0100

    Calibrated extruder steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -542,7 +542,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.765, 79.765, 399.2901, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.765, 79.765, 399.2901, 427.09827 }
 
 /**
  * Default Max Feed Rate (mm/s)
